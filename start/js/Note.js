c5474e225b5750cccb4a21fceb416d9ed1560cbd
Elsis Sitati <sitatielsis@gmail.com>
2017-05-24 19:24:17 +0300

    added state

--- start/js/Note.js
+++ start/js/Note.js
@@ -1,24 +1,52 @@
 var Note = React.createClass({
 
-    edit: function(){
-        alert("editing note");
+    getInitialState: function () {
+        return {editing: false}
     },
-    remove: function(){
+    edit: function () {
+        this.setState({editing: true});
+    },
+    save: function () {
+        this.setState({editing: false});
+    },
+    remove: function () {
         alert("removing note");
     },
-    render: function () {
+    // show content of our notes
+    renderDisplay: function () {
         return (
             <div className="note">
                 <p>{this.props.text}</p>
                 <span>
-                    <button onClick={this.edit}
-                    className="btn btn-primary glyphicon glyphicon-pencil"/>
-                    <button onClick={this.remove}
-                    className="btn btn-danger glyphicon glyphicon-trash" />
+                    <button
+                        onClick={this.edit}
+                        className="btn btn-primary glyphicon glyphicon-pencil"/>
+                    <button
+                        onClick={this.remove}
+                        className="btn btn-danger glyphicon glyphicon-trash"/>
                 </span>
             </div>
         );
+    },
+    // allows us to edit opur notes
+    renderForm: function () {
+        return (
+            <div className="note">
+                <textarea defaultValue ={this.props.text} className='form-control'></textarea>
+                <button
+                    onClick={this.save}
+                    className="btn btn-success btn-sm glyphicon glyphicon-floppy-disk onClick={this.save}"/>
+            </div>
+        );
+
+    },
+    render: function () {
+        if (this.state.editing) {
+            return this.renderForm();
+        } else {
+            return this.renderDisplay();
+        }
     }
 });
 
-React.render(<Note text="Hello world" />, document.getElementById('react-container'));
+React.render(<Note text="Hello world"/>, document.getElementById('react-container'));
